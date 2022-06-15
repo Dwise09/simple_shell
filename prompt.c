46053359b5759d5bf5223d054050572684543f02
Dwise09 <101394519+Dwise09@users.noreply.github.com>
2022-06-15 21:24:33 +0100

    Update prompt.c

--- prompt.c
+++ prompt.c
@@ -1,46 +1,19 @@
 #include "shell.h"
 
 /**
- * prompt - get and read input
+ * prompt - get input
  *
- * Return: exit status
+ * Return: void
  */
 
-int prompt(void)
+void prompt(void)
 {
 char *line = NULL;
 size_t len = 0;
 ssize_t line_size = 0;
-int i, compare;
-char *env = "env";
 
-while (line_size != EOF)
-{
-	if (isatty(STDIN_FILENO))
-		write(1, ":) ", 3);
+write(1, "#cisfun$ ", 9);
+line_size = getline(&line, &len, stdin);
 
-	line_size = _getline(&line, &len, stdin);
-
-	/* compares if input is env and prints env variables if true */
-	compare = _strncmp(env, line, 3);
-	if (compare == 0)
-	{
-		for (i = 0; environ[i]; i++)
-			printf("%s\n", environ[i]);
-
-		prompt();
-		return (1);
-	}
-
-	if (line_size == -1)
-		break;
-
-	if (line_size != 1)
-		tokenifier(line, line_size - 1);
-
-}
-if (isatty(STDIN_FILENO))
-	write(1, "\n", 1);
-
-return (0);
+read_cmd(line, line_size - 1);
 }
